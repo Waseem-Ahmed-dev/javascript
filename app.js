144cf85313b7b2dba4a06033b11ae62d63f35bad
Waseem-Ahmed-dev <waseemahmwd51@gmail.com>
2025-09-26 07:18:26 -0700

    javacsript edtied

---
+++ app.js
@@ -0,0 +1,36 @@
+var username;
+username=prompt("enter usernme");
+console.log(username);
+alert("welcome ,"+ username );
+
+// second file
+
+var username;
+username=prompt("enter usernme");
+console.log(username);
+var age= prompt("enter age");
+console.log(age);
+console.log(username + " is " + age +" years old ");
+
+// third file
+
+var colour=prompt("favorite colour ?");
+console.log(colour);
+alert("wow " + colour + " is a nice colour !");
+
+//  fourth file 
+
+var num1 = Number(prompt("Enter 1st number"));
+var num2 = Number(prompt("Enter 2nd number"));
+
+console.log("The result of " + num1 + " + " + num2 + " is " + (num1 + num2));
+
+//  fifth file 
+
+var username= prompt("enter name");
+console.log(username);
+var hobby= prompt("enter hobby");
+console.log(hobby);
+var country= prompt("enter country");
+console.log(country);
+alert("Hi, I am  " + username + "  from  " + country + "  and I love  " + hobby);
